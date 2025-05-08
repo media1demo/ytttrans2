1908aaf4f0b967e98918a02c81d0c0faad88c7d8
media1demo <141255145+media1demo@users.noreply.github.com>
2025-05-08 18:24:28 +0530

    Update get_transcript.py

--- api/get_transcript.py
+++ api/get_transcript.py
@@ -9,8 +9,8 @@ from youtube_transcript_api.proxies import WebshareProxyConfig
 from youtube_transcript_api import TranscriptsDisabled, NoTranscriptFound, VideoUnavailable 
 
 # IMPORTANT: Set these as Environment Variables in your Vercel project settings
-PROXY_USERNAME = os.environ.get("PROXY_USERNAME")
-PROXY_PASSWORD = os.environ.get("PROXY_PASSWORD")
+PROXY_USERNAME ="ovzsrnps"
+PROXY_PASSWORD = "ajl5st67thul"
 
 class handler(BaseHTTPRequestHandler):
 
